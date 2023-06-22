b4b4070bb034d30b4edd063be987e25ca650afdf
Julio Olvera <julzzdev@gmail.com>
2023-06-22 14:31:02 -0600

    feat(project): created initial project structure

---
+++ apps/expenses-tracker/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+async function fetchData() {
+  const response = await fetch('/api')
+  const data = await response.text()
+  console.log(data)
+}
+</script>
+
+<template>
+  <button @click="fetchData">Click Me</button>
+</template>
+
+<style scoped></style>
